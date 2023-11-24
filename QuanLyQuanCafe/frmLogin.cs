f912e8509730918f646dee04fb9c1ad010f59f90
admin <admin@DESKTOP-VUF9G9D>
2023-11-24 08:56:44 +0700

    Messsege box show :)

--- QuanLyQuanCafe/frmLogin.cs
+++ QuanLyQuanCafe/frmLogin.cs
@@ -20,6 +20,7 @@ namespace QuanLyQuanCafe
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
+            MessageBox.Show(":)");
             frmTableManager f = new frmTableManager();
             this.Hide();
             f.ShowDialog();
